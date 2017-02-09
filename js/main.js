77a9cf8518bb885697d7dc2072e9dc2cc378c7e8
Kunal Garg <kunalrgarg@gmail.com>
2017-02-08 19:40:41 -0500

    updated fireworks to explode at a distance

--- js/main.js
+++ js/main.js
@@ -15,6 +15,13 @@ var raycaster;
 var blocker = document.getElementById( 'blocker' );
 var instructions = document.getElementById( 'instructions' );
 var fireworkChildren = [];
+var fireworkChildren1 = [];
+var fireworkChildren2 = [];
+var fireworkChildren3 = [];
+var fireworkChildren4 = [];
+var fireworkChildren5 = [];
+var counter = 0;
+
 
 // http://www.html5rocks.com/en/tutorials/pointerlock/intro/
 var havePointerLock = 'pointerLockElement' in document || 'mozPointerLockElement' in document || 'webkitPointerLockElement' in document;
@@ -115,22 +122,32 @@ function init() {
 	// === LOOK: Setup spheres
 	geometry = new THREE.BoxGeometry( 20, 20, 20 );
 	
-	for ( var i = 0; i < 5; i ++ ) {
+	for ( var i = 0; i < 10; i ++ ) {
 			
-		var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+		var geometry = new THREE.SphereGeometry( 1, 32, 32 );
 		var material = new THREE.MeshLambertMaterial( {color: 0xffff00} );
 		var sphere = new THREE.Mesh( geometry, material );
 		
 		
 		
-		sphere.position.x = Math.floor( Math.random() * 20 - 100 ) * 20;
-		sphere.position.z = Math.floor( Math.random() * 20 - 100 ) * 20;
+		sphere.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+		sphere.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
 
 
 		
 		scene.add( sphere );
 		fireworks.push( sphere );
 	}
+
+	//Test sphere for refrencing position
+	var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+	var material = new THREE.MeshLambertMaterial( {color: 0x777788} );
+	var sphere = new THREE.Mesh( geometry, material );
+	sphere.position.z = 10;
+	sphere.position.x = 10;
+	sphere.position.y = 10;
+
+	scene.add(sphere);
 
 	light.position.set( 0.5, 1, 0.75 );
 
@@ -141,27 +158,160 @@ function init() {
 
 function animate() {
 	requestAnimationFrame( animate );
+	counter++;
+
+		if (counter === 100) {
+			for ( var i = 0; i < 10; i ++ ) {
+					
+				var geometry = new THREE.SphereGeometry( 1, 32, 32 );
+				var material = new THREE.MeshLambertMaterial( {color: 0xffff00} );
+				var sphere = new THREE.Mesh( geometry, material );
+				
+				
+				
+				sphere.position.x = Math.floor( Math.random() * 20 - 10 ) * 20;
+				sphere.position.z = Math.floor( Math.random() * 20 - 10 ) * 20;
+
+
+				
+				scene.add( sphere );
+				fireworks.push( sphere );
+			}
+		}
 
 	fireworks.forEach(function (v){
 		v.position.y += 0.5;
-		if (v.position === 50){
+		if (v.position.y >= 100){
 			var exploded = fireworks.splice(fireworks.indexOf(v), 1);
+			scene.remove(v);
 
-			var geometry = new THREE.SphereGeometry( 5, 32, 32 );
+			var geometry = new THREE.SphereGeometry( 0.5, 25, 25 );
 			var material = new THREE.MeshLambertMaterial( {color: 0xffff00} );
-			var sphere = new THREE.Mesh( geometry, material );
 
-			fireworkChildren
+			//firework children
+			
+			var sphere = new THREE.Mesh( geometry, material );
+			sphere.position.x = v.position.x;
+			sphere.position.y = v.position.y + 5;
+			sphere.position.z = v.position.z;
+			scene.add(sphere);
+			fireworkChildren.push(sphere);
+
+			var sphere1 = new THREE.Mesh( geometry, material );
+			sphere1.position.x = v.position.x - 5;
+			sphere1.position.y = v.position.y + 3;
+			sphere1.position.z = v.position.z;
+			scene.add(sphere1);
+			fireworkChildren1.push(sphere1);
+
+			var sphere2 = new THREE.Mesh( geometry, material );
+			sphere2.position.x = v.position.x - 5;
+			sphere2.position.y = v.position.y + 3;
+			sphere2.position.z = v.position.z;
+			scene.add(sphere2);
+			fireworkChildren2.push(sphere2);
+
+			var sphere3 = new THREE.Mesh( geometry, material );
+			sphere3.position.x = v.position.x;
+			sphere3.position.y = v.position.y - 5;
+			sphere3.position.z = v.position.z;
+			scene.add(sphere3);
+			fireworkChildren3.push(sphere3);
+
+			var sphere4 = new THREE.Mesh( geometry, material );
+			sphere4.position.x = v.position.x + 5;
+			sphere4.position.y = v.position.y - 3;
+			sphere4.position.z = v.position.z;
+			scene.add(sphere4);
+			fireworkChildren4.push(sphere4);
+
+			var sphere5 = new THREE.Mesh( geometry, material );
+			sphere5.position.x = v.position.x + 5;
+			sphere5.position.y = v.position.y + 3;
+			sphere5.position.z = v.position.z;
+			scene.add(sphere5);
+			fireworkChildren5.push(sphere5);
 		}
 	});
 
+	//modifying children
+
 	if (fireworkChildren.length > 0){
 		fireworkChildren.forEach(function (v){
-			v.position.y -= 0.5;
-			v.position.x -= 0.1;
-			v.position.z += 0.1;
+			if (v.position.y >= 50){
+				v.position.y -= 0.5;
+			}
+			else {
+				scene.remove(v);
+				fireworkChildren.splice(fireworkChildren.indexOf(v), 1);
+			}
 		})
 	}
+
+	if (fireworkChildren1.length > 0){
+		fireworkChildren1.forEach(function (v){
+			if (v.position.y >= 50){
+				v.position.y -= 0.5;
+				v.position.x -= 0.5;
+			}
+			else {
+				scene.remove(v);
+				fireworkChildren1.splice(fireworkChildren1.indexOf(v), 1);
+			}
+		})
+	}
+
+	if (fireworkChildren2.length > 0){
+		fireworkChildren2.forEach(function (v){
+			if (v.position.y >= 50){
+				v.position.y -= 0.5;
+				v.position.x -= 0.5;
+			}
+			else {
+				scene.remove(v);
+				fireworkChildren2.splice(fireworkChildren2.indexOf(v), 1);
+			}
+		})
+	}
+
+	if (fireworkChildren3.length > 0){
+		fireworkChildren3.forEach(function (v){
+			if (v.position.y >= 50){
+				v.position.y -= 0.5;
+			}
+			else {
+				scene.remove(v);
+				fireworkChildren3.splice(fireworkChildren3.indexOf(v), 1);
+			}
+		})
+	}
+
+	if (fireworkChildren4.length > 0){
+		fireworkChildren4.forEach(function (v){
+			if (v.position.y >= 50){
+				v.position.y -= 0.5;
+				v.position.x += 0.5;
+			}
+			else {
+				scene.remove(v);
+				fireworkChildren4.splice(fireworkChildren4.indexOf(v), 1);
+			}
+		})
+	}
+
+	if (fireworkChildren5.length > 0){
+		fireworkChildren5.forEach(function (v){
+			if (v.position.y >= 50){
+				v.position.y -= 0.5;
+				v.position.x -= 0.5;
+			}
+			else {
+				scene.remove(v);
+				fireworkChildren5.splice(fireworkChildren5.indexOf(v), 1);
+			}
+		})
+	}
+
 
 
 	// fireworks.forEach(function (v){
